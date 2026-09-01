6a9938fa62dd6e0c149aa6ac7208c5bf92f28ed2
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-09-01 14:53:21 -0700

    Refactor makesite.go to extract post rendering logic into a separate function and improve error handling

--- makesite.go
+++ makesite.go
@@ -14,10 +14,41 @@ type Post struct {
 	Body  string
 }
 
+func renderPost(tmpl *template.Template, path string) error {
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	parts := strings.SplitN(string(content), "\n\n", 2)
+	post := Post{
+		Title: strings.TrimSpace(parts[0]),
+		Body:  strings.TrimSpace(parts[1]),
+	}
+
+	if err := tmpl.Execute(os.Stdout, post); err != nil {
+		return err
+	}
+
+	outputName := strings.TrimSuffix(path, ".txt") + ".html"
+	out, err := os.Create(outputName)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	return tmpl.Execute(out, post)
+}
+
 func main() {
 	file := flag.String("file", "first-post.txt", "name of the .txt file to render")
 	dir := flag.String("dir", "", "directory to scan for .txt files")
 	flag.Parse()
+
+	tmpl, err := template.ParseFiles("template.tmpl")
+	if err != nil {
+		panic(err)
+	}
 
 	if *dir != "" {
 		matches, err := filepath.Glob(filepath.Join(*dir, "*.txt"))
@@ -27,37 +58,15 @@ func main() {
 		for _, m := range matches {
 			fmt.Println(m)
 		}
+		for _, m := range matches {
+			if err := renderPost(tmpl, m); err != nil {
+				panic(err)
+			}
+		}
 		return
 	}
 
-	content, err := os.ReadFile(*file)
-	if err != nil {
-		panic(err)
-	}
-
-	parts := strings.SplitN(string(content), "\n\n", 2)
-	post := Post{
-		Title: strings.TrimSpace(parts[0]),
-		Body:  strings.TrimSpace(parts[1]),
-	}
-
-	tmpl, err := template.ParseFiles("template.tmpl")
-	if err != nil {
-		panic(err)
-	}
-
-	if err := tmpl.Execute(os.Stdout, post); err != nil {
-		panic(err)
-	}
-
-	outputName := strings.TrimSuffix(*file, ".txt") + ".html"
-	out, err := os.Create(outputName)
-	if err != nil {
-		panic(err)
-	}
-	defer out.Close()
-
-	if err := tmpl.Execute(out, post); err != nil {
+	if err := renderPost(tmpl, *file); err != nil {
 		panic(err)
 	}
 }
