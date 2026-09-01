06e774c9eb12cab36fa50954126d72b62124dae2
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-09-01 14:21:04 -0700

    Implement dynamic post rendering by reading from a file and using a template

--- makesite.go
+++ makesite.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"strings"
+	"text/template"
+)
+
+type Post struct {
+	Title string
+	Body  string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	content, err := os.ReadFile("first-post.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	parts := strings.SplitN(string(content), "\n\n", 2)
+	post := Post{
+		Title: strings.TrimSpace(parts[0]),
+		Body:  strings.TrimSpace(parts[1]),
+	}
+
+	tmpl, err := template.ParseFiles("template.tmpl")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := tmpl.Execute(os.Stdout, post); err != nil {
+		panic(err)
+	}
 }
